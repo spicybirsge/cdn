4e2d03edcce75155077548c4fec6fb2ce89ef199
Shaheer Ahamed <92243459+spicybirsge@users.noreply.github.com>
2023-06-22 22:14:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { storage } = require('./storage/storage');
 const multer = require('multer');
 const upload = multer({ storage });
-const cors= require('cors');
+const cors = require('cors');
 const images = require('./database/images')
 const aerect = require('aerect.js');
 const logger = require('morgan')
@@ -19,24 +19,24 @@ app.use(express.urlencoded({ extended: true }));
 
 
 
-app.post('/upload',authorizer, upload.single('image'), async(req, res) => {
-const id = aerect.generateID(10);
-  if(!req.file) {
-     return res.json({error: "Please provide a valid image"})
+app.post('/upload', authorizer, upload.single('image'), async (req, res) => {
+  const id = aerect.generateID(10);
+  if (!req.file) {
+    return res.json({ error: "Please provide a valid image" })
   }
   const path = req.file.path;
-  if(!path) {
-    return res.json({error: "Please provide a valid image"})
+  if (!path) {
+    return res.json({ error: "Please provide a valid image" })
   }
 
   await images.create({
     id: id,
     url: path
-    
+
   })
 
-  return res.json({success: true, url: `https://cdn.shaheerahamed.me/${id}`})
-  
+  return res.json({ success: true, url: `${id}` })
+
 })
 app.get('/status', async (req, res) => {
   res.sendStatus(200)
@@ -44,16 +44,27 @@ app.get('/status', async (req, res) => {
 app.get('/', async (req, res) => {
   res.sendStatus(403)
 });
-app.get('/:id', async(req, res) => {
+app.get('/proxy', async (req, res) => {
+  const url = req.query.url;
+  console.log(url);
+  if (!url) {
+    return res.status(404).send()
+  }
+    return request.get(url).pipe(res);
+})
+app.get('/:id', async (req, res) => {
   const id = req.params.id;
-  const image = await images.findOne({id: id})
-  if(!image) {
-   return res.status(404).send()
-    
+  const image = await images.findOne({ id: id })
+  if (!image) {
+    return res.status(404).send()
+
   }
   var url = image.url;
-  return await request.get(url).pipe(res);
+  return request.get(url).pipe(res);
 })
+
+
+
 
 
 
